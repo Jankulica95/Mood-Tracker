bd0ff177372b2fd24c1c12fd35e5b798207ef7e7
Nikola Jankovic <71924903+Jankulica95@users.noreply.github.com>
2026-01-08 17:14:38 +0100

    Structure refactor

--- Mobile/app/_layout.tsx
+++ Mobile/app/_layout.tsx
@@ -1,16 +1,5 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-
-  return (
-    <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </>
-  );
+  return <Stack />;
 }
